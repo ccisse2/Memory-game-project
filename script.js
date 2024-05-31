034c47703b0eeb39f5c2daea5e733574ccefddee
ccisse2 <cheikh.cisse2024@campus-eni.fr>
2024-05-31 19:30:10 +0200

    # Please enter the commit message for your changes. Lines starting     # with '#' will be ignored, and an empty message aborts the commit.     #     # On branch main     # Your branch is up to date with 'origin/main'.     #     # Changes to be committed:     #	new file:   asset/animauxAnimes/10.webp     #	new file:   asset/animauxAnimes/9.webp     #	deleted:    asset/dinosaures/1.jpg     #	new file:   asset/dinosaures/1.webp     #	deleted:    asset/dinosaures/10.jpg     #	new file:   asset/dinosaures/10.webp     #  	deleted:    asset/dinosaures/2.jpg     #	new file:   asset/dinosaures/2.webp     #	deleted:    asset/dinosaures/3.jpg     #	new file:   asset/dinosaures/3.webp     #	deleted:    asset/dinosaures/4.jpg     #	new file:   asset/dinosaures/4.webp     #	deleted:    asset/dinosaures/5.jpg     #	new file:   asset/dinosaures/5.webp     #	deleted:    asset/dinosaures/6.jpg     #	new file:   asset/dinosaures/6.webp     #	deleted:    asset/dinosaures/7.jpg     #	new file:   asset/dinosaures/7.webp     #	deleted:    asset/dinosaures/8.jpg     #  	new file:   asset/dinosaures/8.webp     #	deleted:    asset/dinosaures/9.jpg     #	new file:   asset/dinosaures/9.webp     #	modified:   profile.html     #	modified:   script.js     #

--- script.js
+++ script.js
@@ -66,14 +66,10 @@ document.addEventListener('DOMContentLoaded', function () {
         } else if (choixMemory.value == 'animauxAnimes') {
           choixImg.src =
             './asset/animauxAnimes/memory_detail_animaux_animes.png';
-        } else if (choixMemory.value == 'Chiens') {
+        } else if (choixMemory.value == 'chiens') {
           choixImg.src = './asset/chiens/memory_details_chiens.png';
         } else if (choixMemory.value == 'dinosaures') {
           choixImg.src = './asset/dinosaures/memory_detail_dinosaures.png';
-        } else if (choixMemory.value == 'memoryLegume') {
-          choixImg.src = './asset/memory-legume/memory_detail.png';
-        } else if (choixMemory.value == 'alphabetScrable') {
-          choixImg.src = './asset/alphabet-scrabble/memory_detail_scrabble.png';
         }
       });
     }
